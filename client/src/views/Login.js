84a28cc799f0b3a69dc7e03ab7f9c414448a87a0
nicholas mondo <nicholas.mondo@appliedtechnology.se>
2019-07-22 15:13:58 +0200

    login logic

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState, useContext } from "react";
 import {Link } from 'react-router-dom';
-
-
 import { AppContext } from "../context/AppContext";
+import { AuthContext } from "../context/AuthContext";
 import PostForm from "../components/PostForm";
 
 function Login() {
+  const auth = useContext(AuthContext); 
+
   const inputs = [
     { type: "text", name: "username", placeholder: "Username" },
     { type: "password", name: "password", placeholder: "Password" },
@@ -13,7 +14,7 @@ function Login() {
 
   return (
     <div>
-      <PostForm apiPath="/api/login" inputs={inputs} />
+      <PostForm apiPath="/api/login" inputs={inputs} afterPost={auth.afterLogin}/>
       <button><Link to="/signup">Signup</Link></button>
 
     </div>
