9fbc2fb30dfca6de6bcd4d0c1cecfba537bd5e8d
nicholas mondo <nicholas.mondo@appliedtechnology.se>
2019-07-22 14:23:15 +0200

    auth done

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,65 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom';
 import './styles/App.css';
-import Signup from './views/Signup';
 import ProductList from './views/ProductList';
+import Signup from './views/Signup';
 import AllTickets from './views/AllTickets';
 import AppContextProvider from './context/AppContext';
 import Ticket from "./components/Ticket";
 import Order from "./views/Order";
 import RestaurantLayout from "./views/RestaurantLayout";
+import Login from "./views/Login"
+import Employees from "./views/Employees"
 
 function App() {
+  const cookieParser = () => {
+    const cookieObj = {};
+    document.cookie.split(';').forEach((item) => {
+      const pair = item.split('=');
+      cookieObj[pair[0]] = pair[1];
+    })
+    return cookieObj;
+  }
+  cookieParser();
+
   return (
     <AppContextProvider>
 
       <div className="App">
-    <Router>
-        <div className="nav-container">
-          <ul className="nav">
-            {/* <li className="nav-links"><Link to="/signup">Signup</Link></li> */}
-            <li className="nav-links"><Link to="/products">Products</Link></li>
-            <li className="nav-links"><Link to="/all-tickets">All Tickets</Link></li>
-            <li className="nav-links"><Link to="/order">New Order</Link></li>
-            <li className="nav-links"><Link to="/restaurantlayout">Restaurant Overview</Link></li>
-          </ul>
-        </div>
+        <Router>
+          <div className="nav-container">
 
-      <Route path="/signup" component={Signup} />
-      <Route path="/products" component={ProductList} />
-      <Route path="/all-tickets" component={AllTickets} />
-      <Route path="/ticket/:id" component={Ticket} />
-      <Route path="/order" component={Order} />
-      <Route path="/restaurantlayout" component={RestaurantLayout} />
-    </Router>
+
+            {cookieParser().user_id ?
+              <ul className="nav">
+                <li className="nav-links"><Link to="/products">Products</Link></li>
+                <li className="nav-links"><Link to="/all-tickets">All Tickets</Link></li>
+                <li className="nav-links"><Link to="/order">New Order</Link></li>
+                <li className="nav-links"><Link to="/restaurantlayout">Restaurant Overview</Link></li>
+                <li className="nav-links"><Link to="/employees">Employees</Link></li>
+              </ul>
+              :
+              <ul className="nav">
+                <li className="nav-links"><Link to="/login">Login</Link></li>
+              </ul>
+            }
+          </div>
+
+          {cookieParser().user_id ?
+
+            <Route path="/employees" component={Employees} />
+            :
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/products" component={ProductList} />
+              <Route path="/all-tickets" component={AllTickets} />
+              <Route path="/ticket/:id" component={Ticket} />
+              <Route path="/order" component={Order} />
+              <Route path="/restaurantlayout" component={RestaurantLayout} />
+            </Switch>
+
+          }
+        </Router>
       </div>
     </AppContextProvider>
 
--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,19 +1,23 @@
 import React, { useEffect, useState, useContext } from "react";
+import {Link } from 'react-router-dom';
+
+
 import { AppContext } from "../context/AppContext";
 import PostForm from "../components/PostForm";
 
-function Signup() {
+function Login() {
   const inputs = [
-    { type: "text", name: "restaurant_name", placeholder: "Restaurant Name" },
     { type: "text", name: "username", placeholder: "Username" },
-    { type: "password", name: "password", placeholder: "Password" }
+    { type: "password", name: "password", placeholder: "Password" },
   ];
 
   return (
     <div>
-      <PostForm apiPath="/api/restaurants" inputs={inputs}  />
+      <PostForm apiPath="/api/login" inputs={inputs} />
+      <button><Link to="/signup">Signup</Link></button>
+
     </div>
   );
 }
 
-export default Signup;
+export default Login;
