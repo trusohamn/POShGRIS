fdf04325d153bf74ba3add9006cd8a299d53adfa
trusohamn <marta.trusohamn@gmail.com>
2019-07-23 09:26:52 +0200

    Updates

---
+++ precache-manifest.23458ac31d511afe1fe218742c029a08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e3e0153a565e6b0ce1b4b4db84dae8",
+    "url": "/POShGRIS/index.html"
+  },
+  {
+    "revision": "45d56bf49b1a168ea001",
+    "url": "/POShGRIS/static/css/main.90f197f7.chunk.css"
+  },
+  {
+    "revision": "30318e42f2e0c4f00abf",
+    "url": "/POShGRIS/static/js/2.7d543b7a.chunk.js"
+  },
+  {
+    "revision": "45d56bf49b1a168ea001",
+    "url": "/POShGRIS/static/js/main.a95aec83.chunk.js"
+  },
+  {
+    "revision": "97f71d733cf5ee5ab571",
+    "url": "/POShGRIS/static/js/runtime~main.17dfa8e6.js"
+  }
+]);
