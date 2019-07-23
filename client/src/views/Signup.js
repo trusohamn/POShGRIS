7ae211c8ac016b3272cb45e08b2894d84f3f252f
sahin duman <sahin.duman@appliedtechnology.se>
2019-07-23 16:43:07 +0200

    sorted routing on signup

--- client/src/views/Signup.js
+++ client/src/views/Signup.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState, useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import { AuthContext } from "../context/AuthContext";
 import PostForm from "../components/PostForm";
 
-function Signup() {
+function Signup(props) {
+  const auth = useContext(AuthContext); 
+  const context = useContext(AppContext);
+
   const inputs = [
     { type: "text", name: "restaurant_name", placeholder: "Restaurant Name" },
     { type: "text", name: "username", placeholder: "Username" },
@@ -10,9 +14,16 @@ function Signup() {
     { type: "password", name: "password", placeholder: "Password" }
   ];
 
+  const afterPost = (res) => {
+    if (res.error) return ; 
+    auth.afterLogin();
+    props.history.push('/restaurantlayout');
+    context.getRestaurantName();
+  }
+
   return (
     <div>
-      <PostForm apiPath="/api/restaurants" inputs={inputs}  />
+      <PostForm apiPath="/api/restaurants" inputs={inputs} afterPost={afterPost}  />
     </div>
   );
 }
