457ef4b894674f37a7c1c73a835e32bd006d24db
pivey <peteriveyhansen@gmail.com>
2019-07-25 11:16:56 +0200

    added css to app

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -34,20 +34,23 @@ function PostForm(props) {
         <div className="login-form">
           {props.inputs.map(e => {
             if (e.type === 'select') {
-              return <select name={e.name}>
+              return <select name={e.name} className="role-select">
                 {e.options.map(option => {
-                  return <option value={option}>{option}</option>
+                  return <option value={option} >{option}</option>
                 })}
                 </select>
             } else {
               return (
-                <input type={e.type} name={e.name} placeholder={e.placeholder} />
+                <div className="form-group">
+                <input type={e.type} name={e.name} className="form-control" autocomplete="off" required/>
+                <label class="form-control-placeholder" for="name">{e.placeholder}</label>
+                </div>
               );
             }
 
           })}
         </div>
-        <input type='submit' className="submitButton"></input>
+        <input type='submit' className="submitButton" value="Login"></input>
       </div>
     </form>
   );
